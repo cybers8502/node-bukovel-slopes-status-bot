a6557ad68c0b13f670ef5019e07b84ecf8572cce
max.tkhorenko <max.tkhorenko@alty.co>
2025-09-23 00:03:00 +0300

    added icons inform

--- app/utils/fetchSlopesExternalData.js
+++ app/utils/fetchSlopesExternalData.js
@@ -4,14 +4,9 @@ const {SLOPES_STATUS_URL} = require('../configs/consts');
 const logger = require('./logger');
 
 async function fetchSlopesExternalData() {
-  if (process.env.NODE_ENV === 'production') {
-    const response = await axios.get(SLOPES_STATUS_URL);
-    return response.data;
-  } else {
-    const data = await fs.readFile('mockups/slopesData.json', 'utf-8');
-    return JSON.parse(data);
-  }
   logger.info('fetchSlopesExternalData utility in progress');
+  const response = await axios.get(SLOPES_STATUS_URL);
+  return response.data;
 }
 
 module.exports = {fetchSlopesDataService: fetchSlopesExternalData};
